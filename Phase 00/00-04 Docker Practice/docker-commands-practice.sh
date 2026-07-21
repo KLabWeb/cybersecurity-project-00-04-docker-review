622ce5c2d87ebdde74b6caf1355492cff3c96279
Kyle Miskell <kmiskell@protonmail.com>
2026-07-21 11:07:45 -0400

    re-organize repo to be for all projects in study guide, not just docker work. Also more work done with docker commands as well as Dockerfile creation

--- Phase 00/00-04 Docker Practice/docker-commands-practice.sh
+++ Phase 00/00-04 Docker Practice/docker-commands-practice.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 
-# provisioning script for local docker WebGoat install via containers for WebGoat
-# plus nginx & postgres, for further Docker review
+# Provisioning script for local docker WebGoat install via containers for WebGoat
+# Plus nginx & postgres, for further Docker review
 
-# get docker version
+# Get docker version
 docker version
 
-# pull test image & verify pull
+# Pull test image & verify pull
 docker pull nginx:latest
 docker image ls
 
-# start container w/ custom name & verify running
+# Start container w/ custom name & verify running
 docker run -d --name nginx-test nginx 
 docker ps
 ps -ef | grep nginx
 
-# attach container to terminal
+# Attach container to terminal
 echo "Enter container ID then press enter to attach to container"
 read $container_id
 docker attach $container_id
 docker stop $container_id
 
-# run container w/ env var & verify var set
+# Run container w/ env var & verify var set
 docker pull mysql
 docker run -d --name mysql-test --env MYSQL_RANDOM_ROOT_PASSWORD=yes mysql
 docker ps
 docker logs mysql-test 2>&1 | grep "GENERATED ROOT PASSWORD"
 
-# pull and run older version mysql and latest ver mysql in diff containers
+# Pull and run older version mysql and latest ver mysql in diff containers
 docker pull mysql:8.4
 docker run -d --name mysql-old  --env MYSQL_RANDOM_ROOT_PASSWORD=yes mysql:8.4
 docker ps
 
-# via image details for mysql
+# Via image details for mysql
 docker inspect mysql
 
-#stop, pause, and kill containers
+#Stop, pause, and kill containers
 docker stop mysql-test
 docker pause mysql-old
 docker ps
 docker kill mysql-old
 docker ps
 
-# remove mysql 8.4 container & image
+# Remove mysql 8.4 container & image
 docker  ps -a
 docker rm mysql-old
 docker container ps -a
 
-# list images & remove mysql 8.4 image
+# List images & remove mysql 8.4 image
 docker image ls
 docker rmi mysql:8.4
 docker image ls
 
-# via container top processes, HW usage stats, metadata, & logs
+# Via container top processes, HW usage stats, metadata, & logs
 docker top nginx-test
 docker stats nginx-test
 docker inspect nginx-test
 docker logs nginx-test
 
-# run nginx container in interactive mode
+# Run nginx container in interactive mode
 docker stop nginx-test
 docker rm nginx-test
 docker run --name nginx-test -it nginx bash
@@ -66,8 +66,8 @@ ls
 exit
 docker ps
 
-# start container in standard mode, then start in interactive using commands
-# not how I can now exit bash without container stopping, unlike when starting interactive
+# Start container in standard mode, then start in interactive using commands
+# Not how I can now exit bash without container stopping, unlike when starting interactive
 docker rm nginx-test
 docker run -d --name nginx-test nginx
 docker exec -it nginx-test bash
@@ -75,13 +75,13 @@ ls
 exit
 docker ps
 
-# start nginx with bridge between ports 8080 on host and 80 on container & verify port mapping
+# Start nginx with bridge between ports 8080 on host and 80 on container & verify port mapping
 docker stop nginx-test
 docker rm nginx-test
 docker run -d --name nginx-test -p 8080:80 nginx
 docker port nginx-test
 
-# explore networks, then create network and connect nginx-test to it, verify, then disconnect nginx, and remove network in two ways
+# Explore networks, then create network and connect nginx-test to it, verify, then disconnect nginx, and remove network in two ways
 docker network ls
 docker network inspect bridge
 docker network create test-network
@@ -97,16 +97,16 @@ docker network create test-network
 docker network rm test-network
 docker network ls
 
-# remove all stopped containers, networks w/ no containers, dangling images, & build cache
+# Remove all stopped containers, networks w/ no containers, dangling images, & build cache
 docker ps -a
 docker image ls
 docker system prune
 docker ps -a
 
-# view history for mysql image
+# View history for mysql image
 docker history mysql
 
-# pull OWASP WebGoat image using organization name in pull tag, then add custom tag to the iamge
+# Pull OWASP WebGoat image using organization name in pull tag, then add custom tag to the iamge
 docker pull webgoat/webgoat
 docker tag webgoat/webgoat special-webgoat
 docker images | grep webgoat
